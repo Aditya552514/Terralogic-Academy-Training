8b3bf3f1588731e53d9c40486ec4da6ceac3e3c3
Vijay Aditya <vijaygunji2004@gmail.com>
2025-11-02 12:41:03 +0530

    surprise

---
+++ form-validations-app/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import CustomForm from './CustomForm';
+import FormWithErrors from './FormWithErrors';
+import MyForm from './MyForm';
+import MyInput from './MyInput';
+
+function App() {
+  return (
+    <>
+     
+      {/* <MyInput label ="Name:"/>
+      <MyForm/> */}
+      {/* <CustomForm/> */}
+      <FormWithErrors/>
+    </>
+  );
+}
+
+export default App;

24f98233859237259f16509bdc8faf75a5b35170
Vijay Aditya <vijaygunji2004@gmail.com>
2025-11-09 12:18:53 +0530

    new changes

---
+++ custom-hook/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import UserList from './components/UserList';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Custom Hook Example</h1>
+     <UserList/>
+    </div>
+  );
+}
+
+export default App;
